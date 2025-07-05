8fe6b9c485187af4ad0b359d38a9629e695eb2df
itssShiva <iitts.shiva@gmail.com>
2025-07-05 08:44:39 +0530

    Create pallindrome.cpp
    pallindrom program added

---
+++ pallindrome.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool checkPalindrome(int n) {
+    int original = n;
+    int digit;
+    int reversedNum = 0;
+    if(n<0)
+    return false;
+    else{
+
+        while (n != 0) {
+        digit = n % 10;
+
+        // Overflow check
+        if (reversedNum > INT32_MAX / 10 || (reversedNum == INT32_MAX / 10 && digit > 7))
+            return false;
+        if (reversedNum < INT32_MIN / 10 || (reversedNum == INT32_MIN / 10 && digit < -8))
+            return false;
+
+        reversedNum = reversedNum * 10 + digit;
+        n /= 10;
+    }
+    return original == reversedNum;
+    }
+    
+}
+
+int main() {
+    int n = 10;
+    bool res = checkPalindrome(n);
+    cout << (res ? "True" : "False") << endl;
+    return 0;
+}

adc5c12a8c7e9db76c4846df5240f6479ae084f0
itssShiva <iitts.shiva@gmail.com>
2025-07-05 09:43:41 +0530

    Update countDigits.cpp

--- countDigits.cpp
+++ countDigits.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int reverseDigit(int n){
     int count=0,digit;
     while(n!=0){
-        digit=n%10;
         count++;
         n/=10;
     }
     return count;
 }
 int main(){
-    int n=45646;
+    int n=45646545;
     int res=reverseDigit(n);
     cout<<res;
 }
